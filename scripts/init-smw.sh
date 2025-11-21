46cafecb0511bd2c276b54a20bd1906519c7f2f7
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2025-11-20 18:45:31 -0800

    remove redundant update steps

--- scripts/init-smw.sh
+++ scripts/init-smw.sh
@@ -6,11 +6,3 @@ echo "[install] Running maintenance/update.php (first pass)"
 php maintenance/update.php --quick --conf config/LocalSettings.php || true
 
 php extensions/SemanticMediaWiki/maintenance/setupStore.php --skip-optimize --conf config/LocalSettings.php
-
-echo "[install] Running maintenance/update.php (second pass)"
-php maintenance/update.php --quick --conf config/LocalSettings.php || true
-
-if [ -d extensions/SemanticMediaWiki ]; then
-  echo "[install] Running SMW setupStore.php to finalize schema"
-  php extensions/SemanticMediaWiki/maintenance/setupStore.php --skip-optimize --conf config/LocalSettings.php
-fi
